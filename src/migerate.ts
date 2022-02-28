ba5b531fdddd4ecf823fa1bcce9cbad88d63da98
Haris Ahmad <40364018+Haris188@users.noreply.github.com>
2022-02-27 17:33:41 -0800

    Update migerate.ts

--- src/migerate.ts
+++ src/migerate.ts
@@ -1,135 +1,84 @@
-import knex from 'knex'
-import {ConnectionType} from './Db'
-import * as dotenv from 'dotenv'
+import knex from "knex";
+import { ConnectionType } from "./Db";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 let con: ConnectionType = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB
-}
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
+};
 
-let pg = knex({
-    client: 'mysql',
-    connection: {...con}
-});
+// let mysql = knex({
+//     client: 'mysql',
+//     connection: {...con}
+// });
 
 const dataTypes = {
-    string: 'string',
-    increments: 'increments',
-    timestamp:'timestamp',
-    integer:'integer'
+  string: "string",
+  increments: "increments",
+  timestamp: "timestamp",
+  integer: "integer",
+};
+
+const sqlDataTypes = {
+  string: (size=null) => size?`VARCHAR(${size})`: 'VARCHAR'
 }
 
 const tables = [
-    {
-        name: 'users',
-        columns: {
-            id: {type: dataTypes.increments,},
-            first_name: {type: dataTypes.string, nullable: false},
-            last_name: {type: dataTypes.string, nullable: false},
-            email: {type: dataTypes.string, nullable: false},
-            password: {type: dataTypes.string, nullable: false}
+  {
+    name: "users",
+    columns: {
+      id: { type: dataTypes.increments },
+      first_name: { type: dataTypes.string, nullable: false },
+      last_name: { type: dataTypes.string, nullable: false },
+      email: { type: dataTypes.string, nullable: false },
+      password: { type: dataTypes.string, nullable: false },
+    },
+  },
+];
+
+async function migerate() {
+  const mysql = knex({ client: "mysql", connection: con });
+
+  tables.forEach((table, index) => {
+    mysql.schema.hasTable(table.name).then(async (exists) => {
+      console.log('current table:', table.name)
+      if (exists) {
+        for (const col in table.columns) {
+          mysql.schema.hasColumn(table.name, col).then(async (colExists) => {
+            if (!colExists) {
+              const columnData = table.columns[col];
+              await mysql.raw(`ALTER TABLE ${process.env.DB}.${table.name} ADD COLUMN ${col} ${sqlDataTypes[columnData.type](columnData.typeSize)}`)
+              console.log('column added:', col)
+            }
+          })
         }
-    }
-]
+      }
+      else {
+        await mysql.schema.createTable(table.name, (t) => {
+          for (const col in table.columns) {
+            const columnData = table.columns[col];
+            let columnCreated
 
-async function migerate(){
-    pg.destroy();
-
-    con.database = process.env.DB;
-    
-    pg = knex({ client: 'pg', connection: con });
-
-    tables.forEach(table=>{
-        pg.schema.hasTable(table.name).then (async (exists)=>{
-            if(exists){
-                console.log('table exists')
+            if(columnData.typeSize){
+              columnCreated = t[columnData.type](col, columnData.typeSize);
             }
             else{
-                await pg.schema.createTable(table.name, (t)=>{
-                    for(const col in table.columns){
-                        const columnData = table.columns[col]
-                        const columnCreated = t[columnData.type](col)
-                        if(!columnData.nullable) 
-                            columnCreated.notNullable()
-                    }
-                })
+              columnCreated = t[columnData.type](col);
             }
-        })
-    })
 
-    // pg.schema.hasTable('users').then(async function(exists) {
-    //     if (!exists) {
-    //         const tableName = 'users'
-    //         await pg.schema.createTable(tableName, function (table) {
-    //             pg.schema.hasColumn(tableName,'id').then(colExists=>colExists && table.increments('id'))
-    //             pg.schema.hasColumn(tableName,'first_name').then(colExists=>colExists && table.increments('first_name'))
-    //             pg.schema.hasColumn(tableName,'last_name').then(colExists=>colExists && table.increments('last_name'))
-    //             pg.schema.hasColumn(tableName,'email').then(colExists=>colExists && table.increments('email'))
-    //             pg.schema.hasColumn(tableName,'password').then(colExists=>colExists && table.increments('password'))
-    //             // table.increments('id')
-    //             // table.string('first_name').notNullable()
-    //             // table.string('last_name').notNullable()
-    //             // table.string('email').notNullable()
-    //             // table.string('password').notNullable()
-    //         }).then((value)=>{
-    //             console.log('-users created')
-    //         })
-    //     }
-    //   });
-
-    await pg.destroy()
+            if (!columnData.nullable) columnCreated.notNullable();
+            console.log('column created:', col)
+          }
+        });
+      }
+    });
+  });
 }
 
-migerate()
+migerate();
 
-// pg.raw(`CREATE DATABASE ${process.env.DB}`).then(async function () {
-//     pg.destroy();
 
-//     con.database = process.env.DB;
-    
-//     pg = knex({ client: 'pg', connection: con });
-
-//     tables.forEach(table=>{
-//         pg.schema.hasTable(table.name).then (async (exists)=>{
-//             if(exists){
-//                 console.log('table exists')
-//             }
-//             else{
-//                 await pg.schema.createTable(table.name, (t)=>{
-//                     for(const col in table.columns){
-//                         const columnData = table.columns[col]
-//                         const columnCreated = t[columnData.type](col)
-//                         if(!columnData.nullable) 
-//                             columnCreated.notNullable()
-//                     }
-//                 })
-//             }
-//         })
-//     })
-
-//     // pg.schema.hasTable('users').then(async function(exists) {
-//     //     if (!exists) {
-//     //         const tableName = 'users'
-//     //         await pg.schema.createTable(tableName, function (table) {
-//     //             pg.schema.hasColumn(tableName,'id').then(colExists=>colExists && table.increments('id'))
-//     //             pg.schema.hasColumn(tableName,'first_name').then(colExists=>colExists && table.increments('first_name'))
-//     //             pg.schema.hasColumn(tableName,'last_name').then(colExists=>colExists && table.increments('last_name'))
-//     //             pg.schema.hasColumn(tableName,'email').then(colExists=>colExists && table.increments('email'))
-//     //             pg.schema.hasColumn(tableName,'password').then(colExists=>colExists && table.increments('password'))
-//     //             // table.increments('id')
-//     //             // table.string('first_name').notNullable()
-//     //             // table.string('last_name').notNullable()
-//     //             // table.string('email').notNullable()
-//     //             // table.string('password').notNullable()
-//     //         }).then((value)=>{
-//     //             console.log('-users created')
-//     //         })
-//     //     }
-//     //   });
-
-//     await pg.destroy()
-// });
